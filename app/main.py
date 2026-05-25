69d06e5b0b544f8b913ce933bbb32929c163a98b
David Reis <davidreis141435@gmail.com>
2026-05-25 00:07:41 -0300

    indo para passo 4

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
-# app/main.py
-
 from fastapi import FastAPI
-from app.database import Base, engine
-from app.routers import users, assets
 
-Base.metadata.create_all(bind=engine)
+app = FastAPI()
 
-app = FastAPI(title="API REST de Gestão de Arquivos")
-
-app.include_router(users.router, prefix="/auth", tags=["Auth"])
-app.include_router(assets.router, prefix="/assets", tags=["Assets"])
 
 @app.get("/")
-def health_check():
-    return {"status": "API online"}
+def home():
+    return {
+        "message": "Minha primeira API FastAPI"
+    }
